3344f00d9a305259f1ff9fbb29dd4c58697e9226
simona-rp <s0554185@htw-berlin.de>
2023-11-21 16:59:31 +0100

    Update nested json to work with gz

--- reviews.py
+++ reviews.py
@@ -1,13 +1,15 @@
 import pandas as pd
-# import gzip
+import gzip
 import json
 
-# # reads a gzip-compressed file line by line and returns each line as a JSON object
-# def parse(path):
-#   g = gzip.open(path, 'rb')
-#   for l in g:
-#     yield json.loads(l)
-#
+# reads a gzip-compressed file line by line and returns each line as a JSON object
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield json.loads(l)
+
+test = parse('AMAZON_FASHION.json.gz')
+
 # #  creates a DataFrame by converting the JSON data into a dictionary and then into a DataFrame.
 # def getDF(path):
 #   i = 0
@@ -16,21 +18,15 @@ import json
 #     df[i] = d
 #     i += 1
 #   return pd.DataFrame.from_dict(df, orient='index')
-#
-# # read data from a gzip-compressed JSON file to create DataFrame
-# #data = getDF('AMAZON_FASHION.json.gz')
+
+# read data from a gzip-compressed JSON file to create DataFrame
+#data = getDF('AMAZON_FASHION.json.gz')
 # data = getDF('AMAZON_FASHION.json.gz')
 # print(data)
 
-# Load json file
-# Loading a json file keeps the list/dictionary quality, and we can use json_normalize on it
-data = []
-with open('AMAZON_FASHION.json', 'r') as file:
-  for line in file:
-    data.append(json.loads(line))
-
 # Extract level 0 data
-reviews = pd.json_normalize(data)
+reviews = pd.json_normalize(test)
+# Remove blank spaces and non-alphabetic characters from column names
 reviews.columns = reviews.columns.str.replace(r'\W+', '', regex=True)
 reviews.columns = reviews.columns.str.lower()
 pd.set_option('display.max_columns', None)
